cb30b673b0373b25aac11dc400eb1c8cd1875938
elagost <me@elagost.com>
2020-07-21 07:50:51 -0700

    remove leading zeroes so compare works fine

--- isthelighton.sh
+++ isthelighton.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 turnon(){
-
+	curdate=$(date +%H%M|sed 's/^0*//')
 	echo "turn the light on"
-	if [[ $(date +%H) -ge 19 || $(date +%H) -le 6 ]];then
-		echo "it's nighttime between 19:00 and 7:00"
+	if [[ $curdate -ge 1830 || $curdate -le 650 ]];then
+		echo "it's nighttime between 18:30 and 6:50"
 		python3 /home/pi/asleep.py&
-	elif [[ $(date +%H) -ge 7 && $(date +%H) -le 19 ]];then
+	elif [[ $curdate -ge 651 && $curdate -le 1829 ]];then
 		echo "it's daytime between 7 and 19:00"
 		python3 /home/pi/awake.py&
 	fi
@@ -17,5 +17,6 @@ if [[ -z $(ps -ef|grep -v grep|grep -e awake.py -e asleep.py) ]];then
 	echo "process not running"
 	turnon
 else
+	echo "$(ps -ef|grep -v grep|grep -e awake.py -e asleep.py)"
 	echo "it's running! bye."
 fi
